6c95d14369c126a7044f98a2b98e441227da8b1e
PenguinWraith <43303884+penguinsrule521@users.noreply.github.com>
2022-10-13 10:24:43 -0400

    Update button.js

--- button.js
+++ button.js
@@ -8,23 +8,14 @@ function button(link1, link2) {
   text("Play Again", 60 - 18 * 2.5, 35 + 18 / 2.5)
   fill(255);
   noStroke();
-  rect(10, 120, 100, 50);
+  rect(120, 10, 100, 50);
   textSize(18);
   fill(0);
   text(link1, 170 - 18 * 2.5, 35 + 18 / 2.5)
   fill(255);
   noStroke();
-  rect(10, 230, 100, 50);
+  rect(230, 10, 100, 50);
   textSize(18);
   fill(0);
   text(link2, 280 - 18 * 2.5, 35 + 18 / 2.5)
-}
-
-function mousePressed() {
-  if (mouseX > 10
-      && mouseX < 110
-      && mouseY > 10
-      && mouseY < 60) {
-    window.location.href = link;
-  }
 }
