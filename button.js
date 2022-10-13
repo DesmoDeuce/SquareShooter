74bdf7e182cffa904176de0cf62b995274b7930d
PenguinWraith <43303884+penguinsrule521@users.noreply.github.com>
2022-10-13 10:36:56 -0400

    Update button.js

--- button.js
+++ button.js
@@ -2,20 +2,20 @@
 function button(link1, link2) {
   fill(255);
   noStroke();
-  rect(10, 10, 100, 50);
+  rect(10, 10, 120, 50);
   textSize(18);
   fill(0);
-  text("Play Again", 60 - 18 * 2.5, 35 + 18 / 2.5)
+  text("Play Again", 70 - 18 * 2.5, 35 + 18 / 2.5)
   fill(255);
   noStroke();
-  rect(120, 10, 100, 50);
+  rect(140, 10, 120, 50);
   textSize(18);
   fill(0);
-  text(link1, 170 - 18 * 2.5, 35 + 18 / 2.5)
+  text(link1, 190 - 18 * 2.5, 35 + 18 / 2.5)
   fill(255);
   noStroke();
-  rect(230, 10, 100, 50);
+  rect(270, 10, 120, 50);
   textSize(18);
   fill(0);
-  text(link2, 280 - 18 * 2.5, 35 + 18 / 2.5)
+  text(link2, 320 - 18 * 2.5, 35 + 18 / 2.5)
 }
